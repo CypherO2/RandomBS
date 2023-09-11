f0db1cfc303f17067ebf5ba1d7b12783f15ef776
CJ Presley <charlespresley2006@outlook.com>
2023-09-11 19:20:45 +0100

    added Main Menu function for Pokedex

--- Python Code/Code/pokedex.py
+++ Python Code/Code/pokedex.py
@@ -1,9 +1,20 @@
 #pokedex test 1
 entries = []
+choices1 = ["1","2","3","4"]
 
 file = open("Python Code\Code\pokemon.txt","r")
 for el in file:
     entry = el.split("\n")
-    print(entry)
+    entries.append(entry)
 
-#def PokeSearch():
+def Menu():
+    print("+------ Main Menu ------+")
+    print("| 1) Search Pokemon     |")
+    print("| 2) Search Type        |")
+    print("| 3) Search Generation  |")
+    print("+-----------------------+")
+    choice = input("Please Select One of the Above \n > > > ")
+    while not choice in choices1: 
+        choice = input("Please ONLY Select One of the Above \n > > > ")
+    return int(choice)
+
